4da1ff9102490bd43c191712dab91087d4a2f56e
Brian Pham <64495104+BPhi-ai@users.noreply.github.com>
2024-10-31 07:49:43 -0700

    removed comments.

--- db/connection.js
+++ db/connection.js
@@ -5,10 +5,6 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_ADDRESS = process.env.DB_ADDRESS;
 const DB_NAME = process.env.DB_NAME;
 
-//temp import
-import Guest from '../api/models/guest.js';
-import MovieWatchlist from '../api/models/moviewatchlist.js';
-
 const connect = async () => {
     try {
         const MONGO_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_ADDRESS}/?retryWrites=true&w=majority&appName=Cluster0`;
